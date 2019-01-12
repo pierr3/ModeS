7b551b1495d8f790c73934824130cb91107a5e0d
Olli <o.gruetzmann@gmail.com>
2019-01-12 10:07:53 +0100

    Version 1.3.8

--- ModeS/version.h
+++ ModeS/version.h
@@ -5,11 +5,11 @@
 struct PluginData
 {
 	const char * PLUGIN_NAME	{ "Mode S PlugIn" };
-	const char * PLUGIN_VERSION { "1.3.7e32" };
+	const char * PLUGIN_VERSION { "1.3.8e32" };
 	const char * PLUGIN_AUTHOR	{ "Pierre Ferran / Oliver Grützmann" };
 	const char * PLUGIN_LICENSE { "GPL v3" };
-	const char * UPDATE_URL		{ "https://raw.githubusercontent.com/ogruetzmann/ModeS/Master/ModeS/ver.txt" };
-	const int VERSION_CODE		{ 907 };
+	const char * UPDATE_URL		{ "https://raw.githubusercontent.com/ogruetzmann/ModeS/master/ModeS/ver.txt" };
+	const int VERSION_CODE		{ 908 };
 };
 
 struct ItemCodes
